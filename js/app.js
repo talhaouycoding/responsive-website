d9cb26e4d52b7198e3cb4abbde90cb150d851e75
talhaouycoding <talhaouycoding@gmail.com>
2021-03-19 17:59:49 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".navbar");
+  header.classList.toggle("scrolled", window.scrollY > 0);
+});
